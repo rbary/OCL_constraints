447abdd74f221eb73f51f3796691af6456652bf0
sirRolton <bary.roland@gmail.com>
2014-11-28 15:29:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+$mkdir ocl
+cd ocl
+/*Mettre les fichiers qu'il faut*/
+$ /opt/use ..../bin/./use /*Lancement de use*/
+> open personnes.use
+> listePersonnes.cmd
---
+++ listePersonnes-1.cmd
@@ -0,0 +1,36 @@
+1. !new Personne('Roger')
+2. !Roger.nom := 'Roger'
+3. !Roger.age := 22
+4. !Roger.sexe := Sexe::Homme
+5. !new Personne('Pierre')
+6. !Pierre.nom := 'Pierre'
+7. !Pierre.age := 22
+8. !Pierre.sexe := Sexe::Homme
+9. !new Personne('Lucille')
+10. !Lucille.nom := 'Lucille'
+11. !Lucille.age := 17
+12. !Lucille.sexe := Sexe::Femme
+13. !new Personne('Simone')
+14. !Simone.nom := 'Simone'
+15. !Simone.age := 45
+16. !Simone.sexe := Sexe::Femme
+17. !new Personne('Gerard')
+18. !Gerard.nom := 'Gerard'
+19. !Gerard.age := 54
+20. !Gerard.sexe := Sexe::Homme
+21. !new Personne('Saturnin')
+22. !Saturnin.nom := 'Saturnin'
+23. !Saturnin.age := 65
+24. !Saturnin.sexe := Sexe::Homme
+25. !new Personne('Tatiana')
+26. !Tatiana.nom := 'Tatiana'
+27. !Tatiana.age := 56
+28. !Tatiana.sexe := Sexe::Femme
+29. !new Personne('Linda')
+30. !Linda.nom := 'Linda'
+31. !Linda.age := 5
+32. !Linda.sexe := Sexe::Femme
+33. !new Personne('Oskar')
+34. !Oskar.nom := 'Oskar'
+35. !Oskar.age := 2
+36. !Oskar.sexe := Sexe::Homme
